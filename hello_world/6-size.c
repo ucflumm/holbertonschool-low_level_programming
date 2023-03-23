da90b34e07cff33edaf7e4c84ff6fe528501e5a0
ucflumm <ucflumm@gmail.com>
2023-03-23 02:34:04 +0000

    changed output type to unsigned int

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	printf("Size of a char %lu byte(s)\n", sizeof(char));
-        printf("Size of a int %lu byte(s)\n", sizeof(int));
-        printf("Size of a long int %lu byte(s)\n", sizeof(long));
-        printf("Size of a long long int %lu byte(s)\n", sizeof(long long));
-        printf("Size of a float %lu byte(s)\n", sizeof(float));
+	printf("Size of a char %du byte(s)\n", sizeof(char));
+        printf("Size of a int %du byte(s)\n", sizeof(int));
+        printf("Size of a long int %du byte(s)\n", sizeof(long));
+        printf("Size of a long long int %du byte(s)\n", sizeof(long long));
+        printf("Size of a float %du byte(s)\n", sizeof(float));
 
 	return (0);
 }
