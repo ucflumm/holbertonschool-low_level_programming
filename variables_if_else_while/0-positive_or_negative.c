aa1ecbc521494a54966c1a160251d89346dc5bff
ucflumm <ucflumm@gmail.com>
2023-04-03 10:50:57 +0000

    added file 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - randomise an int and prints if +,- or zero
+ * Return: 0 if successful, 1 for error
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else
+	{
+		printf("%d is zero", n);
+	}
+	return (0);
+}
