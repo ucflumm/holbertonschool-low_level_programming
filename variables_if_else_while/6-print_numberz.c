7bc9c2a3239dcbf6b087684728c0e012f48f244e
ucflumm <ucflumm@gmail.com>
2023-04-09 12:02:37 +0000

    cleanup code, modified char values

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
+/* Define 0 and 10 ascii code */
+#define A0 48
+#define A10 58
 /**
  * main - randomise an int and prints if +,- or zero
  * Return: 0 if successful, 1 for error
@@ -10,11 +12,8 @@ int main(void)
 {
 	int i = 0;
 
-	while (i != 10)
-	{
+	for (i = A0; i < A10; i++)
 		putchar(i);
-		i++;
-	}
 	putchar('\n');
 	return (0);
 }
