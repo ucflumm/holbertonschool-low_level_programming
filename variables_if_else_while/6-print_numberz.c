06df0803109af7d5c35dc0944b610991157e81ba
ucflumm <ucflumm@gmail.com>
2023-04-09 11:38:02 +0000

    added file 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - randomise an int and prints if +,- or zero
+ * Return: 0 if successful, 1 for error
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i != 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
