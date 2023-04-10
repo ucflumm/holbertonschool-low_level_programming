dcd4b736384f0ebba9fff15e93227e0bc4d21c71
ucflumm <ucflumm@gmail.com>
2023-04-10 10:44:53 +0000

    added file 1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+#define ASCII_LOWER_A 97
+#define ASCII_LOWER_Z 122
+/**
+* print_alphabet - prints the alphabet
+**/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = ASCII_LOWER_A; i <= ASCII_LOWER_Z; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+}
+/**
+* main - does something
+* Return: 0 (hopefully :SSS)
+*
+**/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
