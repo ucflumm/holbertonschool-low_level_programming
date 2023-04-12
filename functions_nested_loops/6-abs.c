f04287d66a0110a0a27e90638a5a24005e48543c
ucflumm <ucflumm@gmail.com>
2023-04-12 12:21:55 +0000

    made betty happy

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * _abs - check absolute value of ;
- * @i - integer to be checked 
+ * @i: integer to be checked
  * Return: absolute value
  **/
 
 int _abs(int i)
 {
 	int n;
+
 	if (i < 0)
 	{
 		n = i * -1;
