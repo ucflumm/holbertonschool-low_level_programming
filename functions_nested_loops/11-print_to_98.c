6b956a2e8ea84e629e2f754083bd1ead2c843ecb
ucflumm <ucflumm@gmail.com>
2023-04-13 03:00:32 +1000

    added files for question 11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers fron n to 98
+ * @n: number to count from
+*/
+#define PRINT_TO_98 98
+
+void print_to_98(int n)
+{
+	int tmp;
+	if (n == PRINT_TO_98) 
+	{
+		_putchar('9');
+		_putchar('8');
+		_putchar('\n');
+	}
+	else
+	{
+		while (n != PRINT_TO_98)
+		{
+			if (n < PRINT_TO_98)
+			{
+				if (n == 0)
+				{
+					_putchar ('0');
+					_putchar (',');
+					_putchar (' ');
+					n++;
+				}
+				else if (n == -10)
+				{
+					_putchar ('-');
+					_putchar ('1');
+					_putchar ('0');
+					_putchar (',');
+					_putchar (' ');
+					n++;
+				}
+				else if (n < 0)
+				{
+					_putchar('-');
+					tmp = n * -1;
+					_putchar (tmp + '0');
+					_putchar (',');
+					_putchar (' ');
+					n++;
+				}
+				else if (n < 10)
+				{
+					_putchar (n + '0');
+					_putchar (',');
+					_putchar (' ');
+					n++;
+				}
+				else if (n > 9)
+				{
+					tmp = n / 10;
+					_putchar (tmp + '0');
+					tmp = n % 10;
+					_putchar (tmp + '0');
+					_putchar (',');
+					_putchar (' ');
+					n++;
+				}
+			}
+			else if (n > PRINT_TO_98)
+			{
+				if (n > 99)
+				{	
+					tmp = n / 100;
+					_putchar (tmp + '0');
+					tmp = n / 10;
+					tmp = tmp % 10;
+					_putchar (tmp + '0');
+					tmp = n % 10;
+					_putchar (tmp + '0');
+					_putchar (',');
+					_putchar (' ');
+					n--;
+				}
+				else
+				{
+					tmp = n / 10;
+					_putchar (tmp + '0');
+					tmp = n % 10;
+					_putchar (tmp + '0');
+					_putchar (',');
+					_putchar (' ');
+					n--;
+				}
+			}
+			
+			
+		}
+		_putchar ('9');
+		_putchar ('8');
+		_putchar ('\n');
+	}
+}
