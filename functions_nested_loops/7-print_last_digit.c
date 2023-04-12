9b5463068916b42511838643793d0b80556bb5a7
ucflumm <ucflumm@gmail.com>
2023-04-12 13:11:19 +0000

    large code changes works now

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
  * print_last_digit - check absolute value of ;
- * @i - integer to be checked 
+ * @i: integer to be checked
  * Return: absolute value
  **/
-
 int print_last_digit(int i)
 {
-	i = 10 % i;
-	_putchar(i);
+
+	if (i == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+
+	i = i % 10;
 	if (i < 0)
 	{
 		i = i * -1;
+		_putchar(i + '0');
 		return (i);
 	}
+	_putchar(i + '0');
 	return (i);
 }
