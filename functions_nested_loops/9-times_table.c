504c6f0b09c33458c2e0ad4c9e9ac873293b2bed
ucflumm <ucflumm@gmail.com>
2023-04-12 15:28:50 +0000

    made betty compliant and happy

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -3,21 +3,21 @@
 /*Defines for readability*/
 #define ARRAY_SIZE 10
 
-/*
-* times_tables - prints a 10x10 grid of the times tables
-*/
-
+/**
+* times_table - prints a 10x10 grid of the times tables
+**/
 void times_table(void)
 {
 	/* int dimension_array[2D_ARRAY_SIZE][2D_ARRAY_SIZE]; */
 	int i, j, result;
-	char int_str[5] ={' ', ' ', ' ', ' ', ' ',};
+	char int_str[5] = {' ', ' ', ' ', ' ', ' '};
+
 	for (i = 0; i < ARRAY_SIZE; i++)
 	{
 		for (j = 0; j < ARRAY_SIZE; j++)
 		{
-			result = i*j;
-			sprintf (int_str, "%d", result);
+			result = i * j;
+			sprintf(int_str, "%d", result);
 			if (int_str[1] == 0)
 				_putchar (' ');
 			_putchar (int_str[0]);
