ee42e3194cb4bd2d3205dd2288d0e3e9caa93127
ucflumm <ucflumm@gmail.com>
2023-04-12 22:20:13 +1000

    added files for question 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - check absolute value of ;
+ * @i - integer to be checked 
+ * Return: absolute value
+ **/
+
+int _abs(int i)
+{
+	int n;
+	if (i < 0)
+	{
+		n = i * -1;
+		return (n);
+	}
+	return (i);
+}
