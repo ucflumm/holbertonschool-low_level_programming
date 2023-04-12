1cd8667f8891e82ece637ddfc8e7c3a8dca6a1a2
ucflumm <ucflumm@gmail.com>
2023-04-12 21:32:58 +1000

    added files for task 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_sign - takes an integer and outputs its polarity
+ * @n: integer to be checked
+ * Return: sign or '\0' on error
+ **/
+int print_sign(int n)
+{
+	if (n > 0)
+		return (43);
+	if (n == 0)
+		return (48);
+	if (n < 0)
+		return (45)
+	else
+		return ('\0')
+}
