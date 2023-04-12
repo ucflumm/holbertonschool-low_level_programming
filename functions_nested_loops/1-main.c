d52277579b453cd0f16dadc0303cf64e9ad383c0
ucflumm <ucflumm@gmail.com>
2023-04-12 16:07:20 +1000

    minor changes

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -18,16 +18,3 @@ int main(void)
 	return (0);
 }
 
-/**
-* print_alphabet - prints the alphabet
-**/
-void print_alphabet(void)
-{
-	int i;
-
-	for (i = ASCII_LOWER_A; i <= ASCII_LOWER_Z; i++)
-	{
-	_putchar(i);
-	}
-	_putchar('\n');
-}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 
 /* FUNCTION PROTOTYPE */
 extern int _putchar(char c);
+extern void print_alphabet(void);
 #endif
