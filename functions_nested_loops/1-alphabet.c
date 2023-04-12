49ebcda49cd5904a2f60462a8782fcead0973a1d
ucflumm <ucflumm@gmail.com>
2023-04-12 15:55:42 +1000

    changed file structure

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,26 +1,7 @@
-#include <stdio.h>
-#include "main.h"
-
-#define ASCII_LOWER_A 97
-#define ASCII_LOWER_Z 122
-
-void print_alphabet(void);
-
-/**
-* main - does something
-* Return: 0 (hopefully :SSS)
-*
-**/
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
 * print_alphabet - prints the alphabet
 **/
+
 void print_alphabet(void)
 {
 	int i;
