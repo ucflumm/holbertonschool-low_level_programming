0a5acd2a1772e12144decf689d07e123df509772
ucflumm <ucflumm@gmail.com>
2023-04-12 21:44:26 +1000

    added files for question 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,11 +7,20 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		return (43);
+	{
+		_putchar(43);
+		return (1);
+	}
 	if (n == 0)
-		return (48);
+	{
+		_putchar(48);
+		return (0);
+	}
 	if (n < 0)
-		return (45)
+	{	
+		_putchar(45);
+		return (-1);
+	}
 	else
-		return ('\0')
+		return ('\0');
 }
