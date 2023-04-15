aefc9492226bdd927ad15a037e9314a91f07fb72
ucflumm <ucflumm@gmail.com>
2023-04-15 22:34:15 +1000

    modified file 2

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
 /**
- * _isdidit - checks if int is a digit
- * @c: number to check
+ * _mul - checks if int is a digit
+ * @a: number to multiply
+ * @b: number to multiply
  * Return: 1 if digit 0 if not
  * 
- ** /
+ **/
+
+int mul(int a, int b)
+{
+	return (a*b);
+}
