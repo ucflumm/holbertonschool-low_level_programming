04d350128aef3c7e0388f3cfc7269793a1986bbf
ucflumm <ucflumm@gmail.com>
2023-04-16 01:00:31 +1000

    modified file 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - checks if int is a digit
+ * @n: number to check
+ * Return: 1 if digit 0 if not
+ *
+ */
+
+void print_triangle(int n)
+{
+	int i, m;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (m = n - i; m > 0; m--)
+			{
+				_putchar (' ');
+			}
+			for (m = n + i; m >= n; m--)
+				_putchar ('#');
+			_putchar ('\n');
+		}
+	}
+}
