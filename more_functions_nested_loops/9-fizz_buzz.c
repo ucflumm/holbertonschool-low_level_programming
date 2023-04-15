0e99514f61a3f49a8baaea815ba9d1e25c6066a5
ucflumm <ucflumm@gmail.com>
2023-04-15 14:46:38 +0000

    moded fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,7 +7,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -18,6 +18,7 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	printf("Buzz");
 	putchar('\n');
 	return (0);
 }
