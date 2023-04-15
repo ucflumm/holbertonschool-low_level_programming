9155d45f6fe82d4603101338cd1b7f107a8f3491
ucflumm <ucflumm@gmail.com>
2023-04-15 22:55:30 +1000

    modified file 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+#define ASCII_DIGIT_LOWER 48
+#define ASCII_DIGIT_UPPER 57
+
+/**
+ * print_numbers - checks if int is a digit
+ * Return: 1 if digit 0 if not
+ **/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = ASCII_DIGIT_LOWER; i <= ASCII_DIGIT_UPPER; i++)
+		if (i != 50 && i != 52)
+			_putchar (i);
+	_putchar ('\n');
+}
