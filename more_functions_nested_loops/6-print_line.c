6a27f60cd22e68d9c6729b22d46270ce5917d29f
ucflumm <ucflumm@gmail.com>
2023-04-15 13:46:29 +0000

    modified file 6

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,7 +10,9 @@
 void print_line(int n)
 {
 	if (n <= 0)
+	{
 		_putchar ('\n');
+	}
 	else
 	{
 		while (n != 0)
@@ -18,6 +20,7 @@ void print_line(int n)
 			_putchar ('_');
 			n--;
 		}
+		_putchar ('\n');
 	}
 
 }
