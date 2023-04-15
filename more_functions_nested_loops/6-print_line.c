76c0797b0baa06f04f71429afb015cb37ff34632
ucflumm <ucflumm@gmail.com>
2023-04-15 23:42:36 +1000

    modified file 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - checks if int is a digit
+ * @c: number to check
+ * Return: 1 if digit 0 if not
+ *
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar ('\n');
+	else
+	{
+		while (n != 0)
+		{
+			_putchar ('_');
+			n--;
+		}
+	}
+
+}
