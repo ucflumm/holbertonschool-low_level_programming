16fe9302770ce40f5c6e59e80df0ed6a93078274
ucflumm <ucflumm@gmail.com>
2023-04-18 17:37:40 +1000

    added file 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * 
+ * swap_int - takes 2 pointers and swaps their variables  
+ * @*a: first pointer
+ * @*b: second pointer
+ * 
+*/
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+	
+}
