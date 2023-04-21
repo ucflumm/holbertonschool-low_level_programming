5b8664f3b04df8a5895ad86c1040dbaffc392711
ucflumm <ucflumm@gmail.com>
2023-04-21 23:24:39 +1000

    changed again

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 	int i, str_length;
 
 	str_length = (strlen(str) / 2);
-	if (str_length % 2 != 0)
+	if (str_length % 2 == 0)
 		str_length++;
 	printf("Str length = %d \n", str_length);
 	for (i = 0; i <= str_length; i++)
