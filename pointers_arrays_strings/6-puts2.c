908fc8c9ea07f63086f109fa67bc9985afd8e7f1
ucflumm <ucflumm@gmail.com>
2023-04-21 23:23:34 +1000

    modified counting mechanics

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,9 @@ void puts2(char *str)
 	int i, str_length;
 
 	str_length = (strlen(str) / 2);
-	str_length++;
+	if (str_length % 2 != 0)
+		str_length++;
+	printf("Str length = %d \n", str_length);
 	for (i = 0; i <= str_length; i++)
 	{
 		_putchar (*str);
