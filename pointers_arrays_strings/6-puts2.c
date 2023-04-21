fb576761bc77052bcda578db0916dd8bb9a4a55e
ucflumm <ucflumm@gmail.com>
2023-04-21 23:13:00 +1000

    changed loop to <=

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 	int i, str_length;
 
 	str_length = (strlen(str) / 2);
-	for (i = 0; i < str_length; i++)
+	for (i = 0; i <= str_length; i++)
 	{
 		_putchar (*str);
 		str += 2;
