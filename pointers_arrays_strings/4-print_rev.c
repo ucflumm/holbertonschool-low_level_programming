65b8a3768c59fbd688c06103edb7e49f58361627
ucflumm <ucflumm@gmail.com>
2023-04-21 21:16:18 +1000

    added file 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/** 
+ * print_rev - prints string in reverse 
+ * @s: pointer to string
+ * 
+ * 
+*/
+
+void print_rev(char *s)
+{
+	int str_length, i;
+
+	str_length = strlen(s);
+	str_length--;
+	printf("String length is %d \n", str_length);
+	s = s + str_length;
+	for (i = str_length; i >= 0; i--)
+	{
+			_putchar(*s);
+			s--;
+	}
+	_putchar ('\n');
+}
