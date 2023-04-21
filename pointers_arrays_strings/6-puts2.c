f6b2bad02fa80d97f2a39130fd36fcaa6ef3af8d
ucflumm <ucflumm@gmail.com>
2023-04-21 23:07:23 +1000

    added file 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * 
+ * puts2 - prints every other character in a string 
+ * @str: pointer to string
+ * 
+ * 
+*/
+
+void puts2(char *str)
+{
+	int i, str_length;
+
+	str_length = strlen(str);
+	for (i = 0; i < str_length; i++)
+	{
+		_putchar (*str);
+		str += 2;
+	}
+}
