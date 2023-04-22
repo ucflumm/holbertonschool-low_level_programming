553cc6e844f1f4a17778417ebac3683e8b34300c
ucflumm <ucflumm@gmail.com>
2023-04-23 06:48:50 +1000

    added file 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * print_array - prints n elements of an array 
+ * @n: number of elements to print
+ * @a: pointer to arrray
+ * 
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < (n - 1))
+		printf(", ");
+	}
+}
