b31c119634b59bea9719f1ef892bdf981f9cea06
ucflumm <ucflumm@gmail.com>
2023-04-23 07:08:44 +1000

    edited -nve logic

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,15 +12,13 @@ void print_array(int *a, int n)
 	int i;
 
 	if (n >= 0)
+		n *= -1;
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n; i++)
-		{
-			if (i < (n - 1))
-				printf("%d, ", a[i]);
-			else
-				printf("%d\n", a[i]);
-		}
+		if (i < (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
 	}
-	else
-		_putchar('\n');
+
 }
