4c99cb21c5f1d4fff95de75f1687a56703ee88cc
ucflumm <ucflumm@gmail.com>
2023-04-23 06:15:05 +1000

    added file 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/** 
+ * puts_half - puts half a string followed by a newline
+ * @str: string to put half
+ * 
+ * 
+*/
+
+void puts_half(char *str)
+{
+	int str_length;
+
+	str_length = strlen(str);
+	if (str_length % 2 != 0)
+	{
+		str_length--;
+	}
+	str_length = str_length / 2;
+
+	str += str_length;
+	while (*str != '\0')
+	{
+		_putchar (*str);
+		str++;
+	}
+}
