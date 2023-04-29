94cdd63f3026ec9ee16294eba16daacb89ac6fd2
ucflumm <ucflumm@gmail.com>
2023-04-30 04:20:24 +1000

    added file 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** 
+ * reverse_array - reverses array 
+ * @a: pointer to someting
+ * @n: sizeof array??
+ * 
+*/
+
+void reverse_array(int *a, int n)
+{
+	const unsigned int array_size = n;
+	int tmp[array_size];
+	*tmp = 0;
+	int i, p;
+	
+	i = n;
+	p = 0;
+	while (i > 0)
+	{
+		tmp[p] = a[i];
+		i--;
+		p++;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,5 +20,6 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *p1, char *p2);
+void reverse_array(int *a, int n);
 
 #endif
