dd08715b93764c7d0d380143cf66ce9cf30fdcb1
ucflumm <ucflumm@gmail.com>
2023-04-29 17:58:13 +0000

    made betty happy and compliant

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/** 
- * _strcmp - compares two strings 
+/**
+ * _strcmp - compares two strings
  * @s1: first string to compare
  * @s2: second string to compare
  * Return: integer comparison
@@ -15,14 +15,12 @@ int _strcmp(char *s1, char *s2)
 	const unsigned char *p2 = (const unsigned char *) s2;
 	unsigned char c1, c2;
 
-	do
-    	{
-			c1 = (unsigned char) *p1++;
-    		c2 = (unsigned char) *p2++;
-    		if (c1 == '\0')
-				return c1 - c2;
-    	}
-	while (c1 == c2);
+	do {
+		c1 = (unsigned char) *p1++;
+		c2 = (unsigned char) *p2++;
+		if (c1 == '\0')
+			return (c1 - c2);
+	} while (c1 == c2);
 
-	return c1 - c2;
+	return (c1 - c2);
 }
