8107bb855b547a68efb9fc29e0f9afe6d83a89be
ucflumm <ucflumm@gmail.com>
2023-04-30 04:48:38 +1000

    asdf

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,25 @@
 
 void reverse_array(int *a, int n)
 {
-	int *tmparray = malloc(n * sizeof(int));
-	int i = n;
+	int tmparray[20] = {0};
+	int i = n - 1;
 	int p = 0;
 	
-	while (i >= 0)
+	while (i > 0)
 	{
 		tmparray[p] = a[i];
+		i--;
+		p++;
+		printf("tmp array %d\n", tmparray[p]);
+	}
+	
+	printf("tmp array 0 = %d\n", *tmparray);
+	i = n;
+	p = 0;
+	while (i > 0)
+	{
+		*a = tmparray[p];
+		a++;
 		i--;
 		p++;
 	}
