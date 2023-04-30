33ec3535bb19527fb3721ba3d84d99b5d4dfbefc
ucflumm <ucflumm@gmail.com>
2023-04-30 16:49:03 +1000

    added task 3 file 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strspn - gets length of a prefix substring 
+ * @s: source string
+ * @accept: bytes to check
+ * Return: number of bytes in the initial segment of s
+ * 
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+  char *s1 = s;
+  const char *c;
+
+  while (*s)
+    {
+      for (c = accept; *c; c++)
+	{
+	  if (*s == *c)
+	    break;
+	}
+      if (*c == '\0')
+	break;
+      s++;
+    }
+
+  return s - s1;
+}
