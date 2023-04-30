6883440d893e7ed1395a3b521ca206042cd06774
ucflumm <ucflumm@gmail.com>
2023-04-30 06:59:45 +0000

    made betty happy and compliant

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strpbrk - searches string for any of a set of bytes 
+ * _strpbrk - searches string for any of a set of bytes
  * @s: source string
  * @accept: bytes to search for
  * Return: pointer to destination
- * 
+ *
 */
 
 char *_strpbrk(char *s, char *accept)
@@ -13,7 +13,8 @@ char *_strpbrk(char *s, char *accept)
 	const char *scanp;
 	int c, sc;
 
-	while ((c = *s++) != 0) {
+	while ((c = *s++) != 0)
+	{
 		for (scanp = accept; (sc = *scanp++) != 0;)
 			if (sc == c)
 				return ((char *)(s - 1));
