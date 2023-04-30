1874afd9dbff4bb2da8cd75790e6b2b796fd5f63
ucflumm <ucflumm@gmail.com>
2023-04-30 06:33:39 +0000

    made betty happy and compliant

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _memcpy - copies memory area 
+ * _memcpy - copies memory area
  * @dest: destination pointer
  * @src: source pointer
  * @n: number of bytes
  * Return: destination pointer
- * 
+ *
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *d = dest;
 	char *s = src;
+
 	while (n--)
 		*d++ = *s++;
-	return dest;
+	return (dest);
 }
