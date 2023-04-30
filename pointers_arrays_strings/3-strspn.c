8a9006a9c33aa0c84430ad6d8a6287bc48c21034
ucflumm <ucflumm@gmail.com>
2023-04-30 06:50:49 +0000

    made betty happy and compliant

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * _strspn - gets length of a prefix substring 
+ * _strspn - gets length of a prefix substring
  * @s: source string
  * @accept: bytes to check
  * Return: number of bytes in the initial segment of s
- * 
+ *
 */
 
 unsigned int _strspn(char *s, char *accept)
 {
-  char *s1 = s;
-  const char *c;
+	char *s1 = s;
+	const char *c;
 
-  while (*s)
-    {
-      for (c = accept; *c; c++)
+	while (*s)
 	{
-	  if (*s == *c)
-	    break;
-	}
-      if (*c == '\0')
+		for (c = accept; *c; c++)
+		{
+			if (*s == *c)
+			break;
+		}
+	if (*c == '\0')
 	break;
-      s++;
-    }
+	s++;
+	}
 
-  return s - s1;
+	return (s - s1);
 }
