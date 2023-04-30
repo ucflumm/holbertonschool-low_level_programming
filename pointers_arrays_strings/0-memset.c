f0d61d4e7bfef43c4ef4bf3b1eea7ae04e6793f3
ucflumm <ucflumm@gmail.com>
2023-04-30 06:22:04 +0000

    made betty happy and compliant

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte. 
- * @s: pointer to area 
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to area
  * @b: byte to be filled
  * @n: number of bytes
- * 
+ * Return: pointer to destination
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-  char *ptr;
-  
-  ptr = s;
-  while (n-- > 0)
-  {
-	*ptr = b;
-	ptr++;
-  }
-  return (s);
+	char *ptr;
+
+	ptr = s;
+	while (n-- > 0)
+	{
+		*ptr = b;
+		ptr++;
+	}
+	return (s);
 }
