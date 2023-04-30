5e0452b575edac1f3cdc74a36876ffc51e13ba93
ucflumm <ucflumm@gmail.com>
2023-04-30 17:06:20 +1000

    added task 3 file 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring 
+ * @haystack: big haystack
+ * @needle: needle to find in haystack
+ * Return: destination pointer
+ * 
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+    register char *a, *b;
+
+    /* First scan quickly through the two strings looking for a
+     * single-character match.  When it's found, then compare the
+     * rest of the substring.
+     */
+
+    b = needle;
+    if (*b == 0) {
+	return (haystack);
+    }
+    for ( ; *haystack != 0; haystack += 1) {
+	if (*haystack != *b) {
+	    continue;
+	}
+	a = haystack;
+	while (1) {
+	    if (*b == 0) {
+		return (haystack);
+	    }
+	    if (*a++ != *b++) {
+		break;
+	    }
+	}
+	b = needle;
+    }
+    return (NULL);
+}
