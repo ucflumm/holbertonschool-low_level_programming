1d4b1b32dd17e070a8ad197fe259cf59c10025e6
ucflumm <ucflumm@gmail.com>
2023-04-30 16:39:51 +1000

    added task 3 file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: source pointer
+ * @c: char to search for
+ * Return: destination pointer
+ *
+*/
+
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+		return (char*)s;
+		}
+	} while (*s++);
+	return (0);
+}
