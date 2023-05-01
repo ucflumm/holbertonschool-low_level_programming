833885d6b3f1e70ed699e37abb3242013b970945
ucflumm <ucflumm@gmail.com>
2023-05-01 21:53:57 +0000

    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

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,14 +2,14 @@
 #include <stddef.h>
 #include <string.h>
 
-/** 
+/**
  * _strcpy - copies a string
- * @dest: destination string 
+ * @dest: destination string
  * @src: source string
  * Return: destination pointer
 */
 
 char *_strcpy(char *dest, char *src)
 {
-	return memcpy (dest, src, strlen (src) + 1);
+	return (memcpy(dest, src, strlen(src) + 1));
 }
