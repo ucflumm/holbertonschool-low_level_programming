57c087094aa844821f2fc10b5f165b7e22bb0daf
ucflumm <ucflumm@gmail.com>
2023-05-02 20:43:02 +0000

    made betty happy and compliant

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
-/** 
- * reverse_array - reverses array 
+/**
+ * reverse_array - reverses array
  * @a: pointer to someting
  * @n: sizeof array??
- * 
+ *
 */
 
 void reverse_array(int *a, int n)
@@ -12,14 +12,14 @@ void reverse_array(int *a, int n)
 	int tmparray[20] = {0};
 	int i = n - 1;
 	int p = 0;
-	
+
 	while (i >= 0)
 	{
 		tmparray[p] = a[i];
 		i--;
 		p++;
 	}
-	
+
 	i = n - 1;
 	p = 0;
 	while (i >= 0)
