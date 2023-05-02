bcbd2bfc2b74523b42c4682ef0b8df4f0a3984b9
ucflumm <ucflumm@gmail.com>
2023-05-02 20:56:40 +0000

    made betty compliant and happy

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -5,16 +5,16 @@
 #define ASC_UPPER_Z 90
 #define CASE_SHIFT 32
 /**
- * string_toupper - makes a string uppercase 
+ * string_toupper - makes a string uppercase
  * @str: string to check
  * Return: pointer to string
- * 
+ *
 */
 
 char *string_toupper(char *str)
 {
 	char *tmp;
-	
+
 	tmp = str;
 	while (*str != '\0')
 	{
