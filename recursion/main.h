f0c980d5fd6a7c1459c33983e5bf78e0f6a60169
ucflumm <ucflumm@gmail.com>
2023-05-08 19:42:14 +1000

    added new tasks

---
+++ recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void _puts_recursion(char *s);
+
+#endif
