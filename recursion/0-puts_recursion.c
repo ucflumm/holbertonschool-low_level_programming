2c05325971ddab956cc15b0174d8748cc6d0b8b2
ucflumm <ucflumm@gmail.com>
2023-05-08 09:45:03 +0000

    made betty happy and compliant

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * puts_recursion -  recursively call puts
+ * _puts_recursion - recursively call puts
  * @s: pointer to string
- * 
- * 
+ *
 */
 
 void _puts_recursion(char *s)
