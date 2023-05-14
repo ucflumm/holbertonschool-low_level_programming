d9eaecaae30c3abd24bbddb049262c0b1c380ae6
ucflumm <ucflumm@gmail.com>
2023-05-15 06:19:22 +1000

    modified question 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -9,4 +9,15 @@
 int factorial(int n)
 {
 
+	if (n == 1)
+		return (n);
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	if (n != 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	return (-2);
 }
