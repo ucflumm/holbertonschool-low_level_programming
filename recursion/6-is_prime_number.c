05bb8a7f7e941d8ac2d96187bf3c80806919eb56
ucflumm <ucflumm@gmail.com>
2023-05-14 22:14:30 +0000

    made betty compliant

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,29 @@
 #include "main.h"
 
 /**
- * is_prime_number - Write a function that returns 1 if the input integer is a prime number, otherwise return 0.
- * @n: 
- * Return: 
- * 
+ * is_prime_number - checks if number is prime number
+ * @n: number to check
+ * Return: return 1 if yes 0 if no
 */
 
 int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return is_prime_helper(n, n - 1);
+	return (is_prime_helper(n, n - 1));
 }
-
-int is_prime_helper(int num, int divisor) 
+/**
+ * is_prime_helper - core functionality of isprimenumber
+ * @num: number passed through
+ * @divisor: divisor to use
+ * Return: result
+*/
+int is_prime_helper(int num, int divisor)
 {
-    if (divisor == 1) 
-    	return 1;
-    else if (num % divisor == 0) 
-        return 0;
-    else 
-        return is_prime_helper(num, divisor - 1);
+	if (divisor == 1)
+		return (1);
+	else if (num % divisor == 0)
+		return (0);
+	return (is_prime_helper(num, divisor - 1));
 
 }
