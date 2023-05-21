154427322d4673a5bca5638e69933e4c0491367d
ucflumm <ucflumm@gmail.com>
2023-05-21 16:37:19 +1000

    added file 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main of args
+ * @argc: number of args
+ * @argv: vector
+ * Return: 0 for success
+*/
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", (argc - 1));
+	return (0);
+}
