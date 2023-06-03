57b828fb0a0b8c66d4f78a2a1f0c5b3ce7b94ab2
ucflumm <ucflumm@gmail.com>
2023-06-04 04:47:56 +1000

    trying som,ething else

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,9 +13,12 @@ char *str_concat(char *s1, char *s2)
 	char *concat;
 	int counter = 0;
 	int firstcount;
-
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	char nullreplace = '\0';
+	
+	if (s1 == NULL)
+		s1 = &nullreplace;
+	if (s2 == NULL)
+		s2 = &nullreplace; 
 	len = _strlen(s1);
 	len += _strlen(s2);
 	len++;
