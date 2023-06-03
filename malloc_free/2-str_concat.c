f900215994c4d8cb91d5c9e2b2f5e7a4e5331e64
ucflumm <ucflumm@gmail.com>
2023-06-04 04:37:26 +1000

    try this

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,5 +9,73 @@
 
 char *str_concat(char *s1, char *s2)
 {
+	int len;
+	char *concat;
+	int counter = 0;
+	int firstcount;
 
+	len = _strlen(s1);
+	len += _strlen(s2);
+	len++;
+	
+
+	concat = malloc(sizeof(char) * len);
+	if (concat == NULL)
+		return (NULL);
+	
+	while (counter < _strlen(s1))
+	{
+		concat[counter] = s1[counter];
+		counter ++;
+	}
+
+	firstcount = counter;
+	counter = 0;
+	do
+	{
+		concat[firstcount] = s2[counter];
+		printf("fc is %d after process concat is %c and s2 is %c\n", firstcount, concat[firstcount], s2[counter]);
+		counter++;
+		firstcount++;
+	} while (firstcount < len);
+	puts(concat);
+	return (concat);
+	
+}
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ * Return: destination pointer
+ *
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
+
+/**
+ *  _strlen - finds the length of a string
+ *  @s: pointer to string
+ *  Return: the size of the string
+**/
+
+int _strlen(char *s)
+{
+	unsigned int sizecount = 0;
+
+	while (*s != '\0')
+	{
+	sizecount++;
+	s++;
+	}
+	return (sizecount);
 }
