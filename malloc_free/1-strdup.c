2f06ef3b1de84b806b5d50e030db18ca71e7465e
ucflumm <ucflumm@gmail.com>
2023-06-04 03:48:31 +1000

    added functions

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -17,3 +17,40 @@ char *_strdup(char *str)
 	_memcpy(copy, str, len);
 	return (copy);
 }
+
+/**
+ * _memcpy - copies memory area
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ * Return: destination pointer
+ *
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	while (n--)
+		*d++ = *s++;
+	return (dest);
+}
+
+/**
+ *  _strlen - finds the length of a string
+ *  @s: pointer to string
+ *  Return: the size of the string
+**/
+
+int _strlen(char *s)
+{
+	unsigned int sizecount = 0;
+
+	while (*s != '\0')
+	{
+	sizecount++;
+	s++;
+	}
+	return (sizecount);
+}
