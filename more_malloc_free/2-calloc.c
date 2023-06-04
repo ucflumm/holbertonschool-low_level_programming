8f45e992bfdcde0be2ea556d57b5edc49b50bfdf
ucflumm <ucflumm@gmail.com>
2023-06-04 23:20:43 +1000

    asdf

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,16 +10,11 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
-	int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);  
 	ptr = malloc (nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	
-	for (i = 0; i < size; i++)
-		ptr[i] = 0x00;
-	
 	return (ptr);
 }
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -6,7 +6,6 @@
 int _strlen(char *s);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
