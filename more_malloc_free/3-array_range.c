ebf3408b25250740347abbc9b741c5b53c86cde3
ucflumm <ucflumm@gmail.com>
2023-06-04 23:48:36 +1000

    asdf

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -9,5 +9,24 @@
 
 int *array_range(int min, int max)
 {
-
+	int *ptr;
+	int size, counter;
+	if (min > max)
+		return (NULL);
+	size = max - min;
+	if (size == 0)
+		size = 1;
+	ptr = malloc(sizeof(int) * size);
+	if (ptr == NULL)
+		return (NULL);
+	if (size == 1)
+	{
+		ptr[0] = min;
+		return (ptr);
+	}
+	for (counter = 0; counter < size; counter++)
+	{
+		ptr[counter] = min + counter;
+	}
+	return (ptr);
 }
