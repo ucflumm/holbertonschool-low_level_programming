4b9e9072f3c86a1ca3377e825197aa6d45d525aa
ucflumm <ucflumm@gmail.com>
2023-06-04 10:44:36 +0000

    betty compliant

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * free_grid - something it does
- * @grid: something to point
- * @height: height of something
+ * free_grid - frees memory allocated in the previous task
+ * @grid: pointer to grid to free
+ * @height: height of grid
 */
 
 void free_grid(int **grid, int height)
 {
 	int i = 0;
-	
+
 	if (grid != NULL && height > 0)
 	{
 		while (i <= height)
