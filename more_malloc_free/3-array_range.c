be5e2919a83091e783a21c7631c1a3884f5ea614
ucflumm <ucflumm@gmail.com>
2023-06-04 23:51:28 +1000

    dfgh

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	size = max - min;
 	if (size == 0)
 		size = 1;
-	ptr = malloc(sizeof(int) * size);
+	ptr = malloc(sizeof(int) * (size + 1));
 	if (ptr == NULL)
 		return (NULL);
 	if (size == 1)
