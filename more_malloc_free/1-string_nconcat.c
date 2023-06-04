023b531a75bc4b8000884c89e42e9970fa3c5f1a
ucflumm <ucflumm@gmail.com>
2023-06-05 02:44:46 +1000

    asdf

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -127,3 +127,20 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: memory to allocate
+ * Return: void pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
