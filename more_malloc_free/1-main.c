6745b6193eefb21f8567656e6e9972f099ff7991
ucflumm <ucflumm@gmail.com>
2023-06-05 02:09:09 +1000

    asdf

--- more_malloc_free/1-main.c
+++ more_malloc_free/1-main.c
@@ -10,9 +10,13 @@
 int main(void)
 {
     char *concat;
+    char *concat1;
 
-    concat = string_nconcat(NULL, "Hello", 0);
+    concat = string_nconcat("Best ", "School !!!", 6);
+    concat1 = string_nconcat(NULL, "Hello", 0);
     printf("%s\n", concat);
+    printf("%s\n", concat1);
     free(concat);
+    free(concat1);
     return (0);
 }
--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,37 +10,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *ptr;
-	int counter = 0;
-	int counter2 = 0;
-	int len = 0;
+	char *ptr, *ptr1;
+	/* int counter = 0; */
+	int len = n;
+	char empty = '\0';
+
+	if (s1 == NULL)
+		s1 = &empty;
+	if (s2 == NULL)
+		s2 = &empty;
 	
-	len = arg_check(s1, s2, n);
-	ptr = malloc(sizeof(char) * (len + 1));
-	if (ptr == NULL)
-		return (NULL);
-	if (*s1 == '\0'  && *s2 == '\0')
+	ptr = malloc_checked(sizeof(char) * (len + 1));
+	if (arg_check(s1, s2, ptr, n))
 	{
-		*ptr = '\0';
+		puts("condition ran true!");
 		return (ptr);
 	}
-	if (*s1 == '\0' && n == 0)
-	{
-		*ptr = '\0';
-		return (ptr);
-	}
-	for (counter = 0; counter < _strlen(s1); counter++)
-		ptr[counter] = s1[counter];
-	counter2 = counter;
-	counter = 0;
-	do {
-		ptr[counter2] = s2[counter];
-		counter++;
-		counter2++;
-	} while (counter < (len - _strlen(s1)));
-	if (*s2 != '\0') 
-		ptr[counter2] = '\0';
-	return (ptr);
+
+	_memcpy(ptr, s1, (_strlen(s1) + 1));
+	ptr1 = &*ptr;
+	while (*ptr != '\0')
+		ptr++;
+	_memcpy(ptr, s2, (n + 1));
+	return (ptr1);
+	
 }
 
 /**
@@ -81,25 +74,26 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 }
 
 /**
- *  arg_check - finds the length of a string
- *  @s: pointer to string
- *  Return: the size of the string
-**/
+ * _memcpy - copies memory area
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes
+ * Return: destination pointer
+ *
+*/
 
-int arg_check(char *s1, char *s2, int n)
+int arg_check(char *s1, char *s2, char *s3, unsigned int n)
 {
-	char nullreplace = '\0';
-	int len;
-
-	if (s1 == NULL)
-		s1 = &nullreplace;
-	if (s2 == NULL)
-		s2 = &nullreplace;
-	if (_strlen(s2) < n)
-		len = _strlen(s2) + _strlen(s1);
-	else if (*s1 == '\0' && n == 0)
-		len = 0;
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		*s3 = '\0';
+		return (1);
+	}
+	if (*s1 == '\0' && n == 0)
+	{
+		*s3 = '\0';
+		return(1);
+	}
 	else
-		len = _strlen(s1) + n;
-	return (len);
+		return (0);
 }
