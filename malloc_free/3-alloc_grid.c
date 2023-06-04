3878d962be4470572d8a201855cbec10916c01c9
ucflumm <ucflumm@gmail.com>
2023-06-04 20:10:26 +1000

    added file 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -8,5 +8,31 @@
 
 int **alloc_grid(int width, int height)
 {
+	int i, n;
+	int **arr = malloc(height * sizeof(int *));
+	
+	for(i = 0; i < height; i++) 
+		arr[i] = malloc(width * sizeof(int));
+	
+	if (width <= 0 || height <= 0)
+	{
+		printf("Width or height is less than 1\n");
+		return (NULL);
+	}
+	
+	if (arr == NULL)
+	{
+		printf("Malloc Fail\n");
+		return (NULL);
+	}
 
+	for (i = 0; i < height; i++)
+	{
+		for (n = 0; n < width; n++)
+		{
+			arr[i][n] = 0;
+		}
+	}
+
+	return (arr);
 }
