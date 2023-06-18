eea6400e5cf12d9a209931f5475a026b5d6829ab
ucflumm <ucflumm@gmail.com>
2023-06-18 21:14:01 +1000

    asdf

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 /**
  * print_dog - does something?
- * d: pointer to dog struct
+ * @d: pointer to dog struct
  */
 void print_dog(struct dog *d)
 {
@@ -12,11 +12,9 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			d->name = "(nil)";
 		printf("Name: %s\n", d->name);
-		/** if (d->age == NULL)
-		*	printf("Age: (nil)\n");
-		* else
-		*/
+		
 		printf("Age: %f\n", d->age);
+		
 		if (d->owner == NULL)
 			d->owner = "(nil)";
 		printf("Owner: %s\n", d->owner);
