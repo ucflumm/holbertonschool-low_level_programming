ce699623f55aa67d67ed2bef479506ab1f76662f
ucflumm <ucflumm@gmail.com>
2023-06-18 11:17:00 +0000

    betty compliant

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,9 +12,9 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			d->name = "(nil)";
 		printf("Name: %s\n", d->name);
-		
+
 		printf("Age: %f\n", d->age);
-		
+
 		if (d->owner == NULL)
 			d->owner = "(nil)";
 		printf("Owner: %s\n", d->owner);
