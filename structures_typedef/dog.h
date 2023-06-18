7f1657d9aafe7a50169f130cd9a9443b3f0ba93d
ucflumm <ucflumm@gmail.com>
2023-06-18 18:52:10 +1000

    added description

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,7 +2,14 @@
 #define MAIN_HEADER
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * struct dog - Short description
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
