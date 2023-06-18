5dd43eac3048e037885e6016dd15b6620dfd7359
ucflumm <ucflumm@gmail.com>
2023-06-19 01:20:49 +1000

    d

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * main - does something?
+ * Return: 0 if successful, 1 for error
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
