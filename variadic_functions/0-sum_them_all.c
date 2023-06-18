25ecb946fc2c00800d9e5c2b332c8d65f41bbad4
ucflumm <ucflumm@gmail.com>
2023-06-19 02:43:27 +1000

    finished draft

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - does something?
+ * Return: 0 if successful, 1 for error
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int sum = 0;
+	
+	if (n == 0)
+		return (0);
+	
+	va_start(ap, n);
+	for(i = 0; i < n; i++)
+	{
+		sum += va_arg(ap, int);
+	}
+
+	return (sum);
+
+}
