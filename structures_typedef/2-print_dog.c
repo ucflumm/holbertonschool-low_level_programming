3ba5d6338b8e0da292ba8ebb415fc48ecaef280d
ucflumm <ucflumm@gmail.com>
2023-06-18 21:12:51 +1000

    fghj

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -12,9 +12,10 @@ void print_dog(struct dog *d)
 		if (d->name == NULL)
 			d->name = "(nil)";
 		printf("Name: %s\n", d->name);
-		// if (d->age == NULL)
-			// printf("Age: (nil)\n");
-		// else
+		/** if (d->age == NULL)
+		*	printf("Age: (nil)\n");
+		* else
+		*/
 		printf("Age: %f\n", d->age);
 		if (d->owner == NULL)
 			d->owner = "(nil)";
