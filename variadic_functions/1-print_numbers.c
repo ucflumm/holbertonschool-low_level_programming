51d47ae20d97ef3a706a8c9288f084e5b8c8319f
ucflumm <ucflumm@gmail.com>
2023-06-19 02:53:44 +1000

    finished coding

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * main - does something?
+ * Return: 0 if successful, 1 for error
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	int num;
+
+	if (n == 0)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		num = va_arg(ap, int);
+		printf("%d", num);
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+	va_end(ap);
+	printf("\n");
+}
