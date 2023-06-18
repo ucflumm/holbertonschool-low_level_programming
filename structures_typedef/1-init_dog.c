b17c945cccb57ac6c80179f8b903f095b7b0fc94
ucflumm <ucflumm@gmail.com>
2023-06-18 19:33:15 +1000

    finished file 1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - does something?
+ * Return: 0 if successful, 1 for error
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+
+	return (d);
+}
+
