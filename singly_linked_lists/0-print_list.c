758f89a338e176981cb222917d7fc1c7a17747a7
Uwe Flumm <ucflumm@gmail.com>
2023-08-10 04:31:25 +1000

    fixed core code logic

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,5 +14,24 @@
 */
 size_t print_list(const list_t *h)
 {
+    size_t count = 0;
+    list_t *current;
+    
+    current = (*h).next;
+    if (h->str != NULL)
+        printf("[%d] %s\n", h->len, h->str);
+    else
+        printf("[0] (nil)\n");
+    count++;
 
+    
+    do {
+        printf("[%d] %s!\n", current->len, current->str);
+        count++;
+        if (current->next == NULL)
+            return count;
+        current = current->next;
+    } while (current != NULL);
+    
+    return (99);
 }
