caf255bff47620e6c1db66c97a8eaf7b45ea45ff
Uwe Flumm <ucflumm@gmail.com>
2023-08-11 10:55:00 +1000

    added to gitignore

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,29 +9,38 @@
 /**
  * print_list - prints the list
  * @h: pointer to head of list
- *
+ * Return: size of elements
  *
 */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    list_t *current;
-    
-    current = (*h).next;
-    if (h->str != NULL)
-        printf("[%d] %.*s\n", h->len, h->len, h->str);
-    else
-        printf("[0] (nil)\n");
-    count++;
+	size_t count = 0;
+	list_t *current;
+	char *safeString = NULL;
 
-    
-    do {
-        printf("[%d] %.*s!\n", current->len, current->len, current->str);
-        count++;
-        if (current->next == NULL)
-            return count;
-        current = current->next;
-    } while (current != NULL);
-    
-    return (99);
+	current = h->next;
+	if (h->str == NULL || current->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		current = h->next;
+		count++;
+	}
+	if (count == 0)
+	{
+		safeString = strndup(h->str, h->len);
+		printf("[%d] %s\n", h->len, safeString);
+		free(safeString);
+		count++;
+	}
+
+	while (current != NULL)
+	{
+		safeString = strndup(current->str, current->len);
+		printf("[%d] %s\n", current->len, safeString);
+		count++;
+		current = current->next;
+		free(safeString);
+	}
+	return (count);
+
 }
