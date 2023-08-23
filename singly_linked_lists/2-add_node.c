5a0a98344bf88980d9ca635f6357b20cb61f358b
Uwe Flumm <ucflumm@gmail.com>
2023-08-24 01:16:15 +1000

    betty comp

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,26 +11,25 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    
-    
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
-    new_node->str = strdup(str);
-    new_node->len = strlen(str);
-    
-    /** Special condition for first node*/
-    if (*head == NULL)
-    {
-        new_node->len = strlen(str);
-        new_node->next = NULL;
-        *head = new_node;
-        return (*head);
-    }
+	list_t *new_node;
 
-    new_node->next = *head;
-    *head = new_node;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 
-    return (*head);
+	/** Special condition for first node*/
+	if (*head == NULL)
+	{
+		new_node->len = strlen(str);
+		new_node->next = NULL;
+		*head = new_node;
+		return (*head);
+	}
+
+	new_node->next = *head;
+	*head = new_node;
+
+	return (*head);
 }
