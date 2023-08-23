1eefc1d22bb4404ba84d3961f257838b298859df
Uwe Flumm <ucflumm@gmail.com>
2023-08-24 00:52:00 +1000

    clean code

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -16,41 +16,29 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 	list_t *current;
-	char *safeString = NULL;
-
+	
 	if (h == NULL)
 		return (count);
-	current = h->next;
-	if (h->next == NULL)
+	
+	if (h->str == NULL)
+	{
+		printf("[0] (nil)\n");;
+		count++;
+	}
+	else
 	{
 		printf("[%d] %s\n", h->len, h->str);
 		count++;
-		return (count);
 	}
-	if (h->str == NULL || current->str == NULL)
-	{
-		printf("[0] (nil)\n");
-		current = h->next;
-		count++;
-	}
-	if (count == 0)
-	{
-		safeString = strndup(h->str, h->len);
-		printf("[%d] %s\n", h->len, safeString);
-		free(safeString);
-		count++;
-		if (h->next == NULL)
-			return (count);
-	}
-
+	
+	current = h->next;
 	while (current != NULL)
 	{
-		safeString = strndup(current->str, current->len);
-		printf("[%d] %s\n", current->len, safeString);
+		printf("[%d] %s\n", current->len, current->str);
 		count++;
 		current = current->next;
-		free(safeString);
 	}
+	
 	return (count);
 
 }
