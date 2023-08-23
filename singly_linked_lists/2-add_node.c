6d71934a124c5c2000d78cee89ab73bbe1f0fcd2
Uwe Flumm <ucflumm@gmail.com>
2023-08-24 00:58:35 +1000

    added str len into struct

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -18,10 +18,12 @@ list_t *add_node(list_t **head, const char *str)
     if (new_node == NULL)
         return (NULL);
     new_node->str = strdup(str);
+    new_node->len = strlen(str);
     
     /** Special condition for first node*/
     if (*head == NULL)
     {
+        new_node->len = strlen(str);
         new_node->next = NULL;
         *head = new_node;
         return (*head);
