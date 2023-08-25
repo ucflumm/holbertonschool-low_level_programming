e3d1b1ad68090ea4ab92af4ae33faea8fd78f185
Uwe Flumm <ucflumm@gmail.com>
2023-08-25 14:48:20 +1000

    betty compliant

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -9,36 +9,36 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
-    list_t *last_node;
+	list_t *new_node;
+	list_t *last_node;
 
-    /* Allocate space for new node */
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate space for new node */
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    /** Special condition for first node*/
-    if (*head == NULL)
-    {
-        new_node->len = strlen(str);
-        new_node->str = strdup(str);
-        new_node->next = NULL;
-        *head = new_node;
-        return (*head);
-    }
+	/** Special condition for first node*/
+	if (*head == NULL)
+	{
+		new_node->len = strlen(str);
+		new_node->str = strdup(str);
+		new_node->next = NULL;
+		*head = new_node;
+		return (*head);
+	}
 
-    /* Assign values for new node's members*/
-    new_node->str = strdup(str);
-    new_node->len = strlen(str);
+	/* Assign values for new node's members*/
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
 
-    /* Allocate last_node to head and iterate through list*/
-    last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+	/* Allocate last_node to head and iterate through list*/
+	last_node = *head;
+	while (last_node->next != NULL)
+	last_node = last_node->next;
 
-    /* Assign last_node's next to new_node and new_node's next to NULL*/
-    last_node->next = new_node;
-    new_node->next = NULL;
+	/* Assign last_node's next to new_node and new_node's next to NULL*/
+	last_node->next = new_node;
+	new_node->next = NULL;
 
-    return (*head);
+	return (*head);
 }
