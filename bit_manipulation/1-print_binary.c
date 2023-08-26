e7ace91451fdcacc4a800caccd5b30546b25e55e
Uwe Flumm <ucflumm@gmail.com>
2023-08-27 02:21:34 +1000

    betty compliant

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: number to be printed in binary
+*/
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1;
+	int flag = 0;
+
+	mask <<= 63;
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask > 0)
+	{
+		if ((n & mask) == 0)
+		{
+			if (flag == 1)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		mask >>= 1;
+	}
+}
