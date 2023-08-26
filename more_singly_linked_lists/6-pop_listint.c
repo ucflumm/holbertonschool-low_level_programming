07265447fa8c25b36a2f0e287d7e44bdcfa2d08c
Uwe Flumm <ucflumm@gmail.com>
2023-08-27 00:55:03 +1000

    betty comp

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ * @head: pointer to pointer to head of list
+ * Return: data in deleted node
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	/* if the list is empty */
+	if (*head == NULL)
+		return (0);
+	/* safely remove the node */
+	temp = *head;
+	n = temp->n;
+	*head = temp->next;
+	free(temp);
+
+	return (n);
+}
