e014aead5b65932ace5e1c71ad593de7169f44de
Uwe Flumm <ucflumm@gmail.com>
2023-08-27 01:15:19 +1000

    hard coded second function

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,36 @@
 #include "lists.h"
+#include "lists.h"
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to head of list
+ * @n: integer to be included in new node
+ * Return: address of new element or NULL if failed
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	if (head == NULL)
+	return (NULL);
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	if (*head == NULL)
+	{
+		new_node->next = NULL;
+		*head = new_node;
+		return (new_node);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
+
 /**
  * listint_len - prints all the elements of a listint_t list
  * @h: pointer to head of list
