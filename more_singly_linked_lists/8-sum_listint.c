861e349b3bd2b6de09cac9862a5201a858a3e92b
Uwe Flumm <ucflumm@gmail.com>
2023-08-27 01:05:44 +1000

    betty comp

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+/**
+ * sum_listint - sum of all the data (n) of a listint_t linked list.
+ * @head: pointer to the head of the list
+ * Return: sum of all the data (n) of a listint_t linked list.
+*/
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+	listint_t *temp = head;
+
+	/* Sanity checks */
+	if (head == NULL)
+		return (0);
+
+	/* Traverse the list */
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
