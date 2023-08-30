f1e8bce1c8da39e51a9533e147ce00a779c96fc2
Uwe Flumm <ucflumm@gmail.com>
2023-08-30 11:45:16 +1000

    betty compliant

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,1 +1,51 @@
+#include "main.h"
 
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: name of the file
+ * @letters: number of letters it should read and print
+ * Return: the actual number of letters it could read and print
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t n_read, n_write;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	n_write = write(STDOUT_FILENO, buffer, n_read);
+	if (n_write == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (n_write);
+}
