f3802eb1ff5bd2bbe1f72651a758835ea1e2bd77
Uwe Flumm <ucflumm@gmail.com>
2023-09-03 03:08:24 +1000

    betty comp

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,5 +1,8 @@
-#ifndef "main_header"
-#define "main_header"
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -7,13 +10,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+		struct dlistint_s *prev;
+		struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
