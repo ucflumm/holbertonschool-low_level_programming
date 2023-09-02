1751e8fe783dbf8c3aca6676265c73778cca882d
Uwe Flumm <ucflumm@gmail.com>
2023-09-03 04:26:19 +1000

    betty compl

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: pointer to the head of the list
+ *
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = NULL;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
