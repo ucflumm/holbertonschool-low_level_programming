f2cce5949453948640e19ba3c85c0c2fbbd8b1e8
Uwe Flumm <ucflumm@gmail.com>
2023-09-18 15:45:28 +1000

    betty compliant

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,25 +1,27 @@
 #include "search_algos.h"
-/*
- * vmlinuz*/
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: pointer to the first element of the array
+ * @size: number of elements in the array
+ * @value: value to search for
+ * Return: the first index where value is located
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
+	/* Sanity Checks */
 	if (array == NULL)
 		return (-1);
-	
+
+	/* Loop through array */
 	while (i < size)
 	{
-		
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return ((int) i);
-		
-		
 		i = i + 1;
-
 	}
-
 	return (-1);
 }
