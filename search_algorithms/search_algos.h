b926a2f0124e52572dd5e11197827980a2b347e5
Uwe Flumm <ucflumm@gmail.com>
2023-09-21 20:46:45 +1000

    removed avg_ceil made static

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -3,7 +3,10 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H END */
 
